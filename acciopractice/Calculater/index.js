99a6bbc8379d2649a7a5652b433ef76ab967f640
Arambh Suyal <arambhsuyal@gmail.com>
2022-04-15 23:50:27 +0530

    project and class

--- acciopractice/Calculater/index.js
+++ acciopractice/Calculater/index.js
@@ -1,6 +1,39 @@
 var displayArea=document.getElementById('display')
 
-function buttonClicked(DigitorOperater)
+function isOperater(buttonText)
 {
-    displayArea.innerText=displayArea.innerText + DigitorOperater
+    if(buttonText == '+' || buttonText == '-' || buttonText == '*' || buttonText == '/' || buttonText == '%')
+    {
+return true
+    }
+    else
+    {
+        return false
+    }
+}
+
+function buttonClicked(buttonText)
+{ 
+    if(buttonText==='0')
+    {
+        if(buttonText !='0' && buttonText !='00')//dont do anything if 0 and 00 is pressed
+    {   
+     
+        if(buttonText =='.' || isOperater(buttonText))
+   //if buttom clicked is . or any operaters, then show it a 0 followed by a button clicked (eg: 0.5, 0+5)
+    {
+        displayArea.innerText= displayArea.innerText +buttonText
+    }
+    
+    else
+    {
+        displayArea.innerText=buttonText
+    }
+    }
+    }
+    
+
+    else{
+    displayArea.innerText=displayArea.innerText + buttonText
+}
 }
