81d5ff0306da17736891d65076d67af12979ca08
Arambh Suyal <arambhsuyal@gmail.com>
2022-04-21 20:20:56 +0530

    new

---
+++ acciopractice/BankInterestManagement/index.js
@@ -0,0 +1,15 @@
+
+let bankBalances = [249389, 34561,3462,242442,100001]
+
+for(let i=0;i<bankBalances.length;i++)
+
+{
+    if(bankBalances[i]>200000)
+    {
+        bankBalances[i]=
+        bankBalances[i]
+        bankBalances[i]
+        
+
+    }
+}
--- acciopractice/Calculater/index.js
+++ acciopractice/Calculater/index.js
@@ -1,39 +1,27 @@
-var displayArea=document.getElementById('display')
+var displayArea = document.getElementById('display')
 
-function isOperater(buttonText)
-{
-    if(buttonText == '+' || buttonText == '-' || buttonText == '*' || buttonText == '/' || buttonText == '%')
-    {
-return true
-    }
-    else
-    {
-        return false
-    }
+function isOperator(buttonText){
+  if(buttonText == '+' || buttonText == '-' || buttonText == '*' || buttonText == '/' || buttonText == '%')
+    return true
+  else 
+    return false
 }
 
-function buttonClicked(buttonText)
-{ 
-    if(buttonText==='0')
-    {
-        if(buttonText !='0' && buttonText !='00')//dont do anything if 0 and 00 is pressed
-    {   
-     
-        if(buttonText =='.' || isOperater(buttonText))
-   //if buttom clicked is . or any operaters, then show it a 0 followed by a button clicked (eg: 0.5, 0+5)
-    {
-        displayArea.innerText= displayArea.innerText +buttonText
+function buttonClicked(buttonText){
+  if(displayArea.innerText === '0'){
+    if(buttonText != '0' && buttonText != '00'){ // Don't do anything if button clicked is 0 or 00
+      if(buttonText == '.' || isOperator(buttonText)){ 
+        //If button clicked is . or any operators, then show it a 0 followed by button clicked (Eg: 0.5, 0+5)
+        displayArea.innerText = displayArea.innerText + buttonText
+      } else {
+        displayArea.innerText = buttonText
+      }
     }
-    
-    else
-    {
-        displayArea.innerText=buttonText
+  } else {
+    if(isOperator(displayArea.innerText[displayArea.innerText.length - 1]) && isOperator((buttonText))){
+      //  replace the operator
+    } else {
+      displayArea.innerText = displayArea.innerText + buttonText
     }
-    }
-    }
-    
-
-    else{
-    displayArea.innerText=displayArea.innerText + buttonText
-}
+  }
 }
