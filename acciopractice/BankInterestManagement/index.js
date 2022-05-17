33906a6c9ba0b4ff00b8abc67668f78cf7a90242
Arambh Suyal <arambhsuyal@gmail.com>
2022-05-17 14:51:39 +0530

    banknew

--- acciopractice/BankInterestManagement/index.js
+++ acciopractice/BankInterestManagement/index.js
@@ -1,15 +1,55 @@
 
-let bankBalances = [249389, 34561,3462,242442,100001]
+//let bankBalances = [249389, 34561,3462,242442,100001]
 
-for(let i=0;i<bankBalances.length;i++)
+//let bankBalances = prompt(" Enter all bank balances separated by a ',' ").split(',')
 
-{
-    if(bankBalances[i]>200000)
-    {
-        bankBalances[i]=
-        bankBalances[i]
-        bankBalances[i]
+
+
+
+// for(let i=0;i<bankBalances.length;i++)
+//         {
+//        bankBalances[i] = Number(bankBalances[i])
+
+//     if(bankBalances[i] > 200000)
+//     {
         
+//         bankBalances[i]=  bankBalances[i]+ bankBalances[i]*0.1
+//         bankBalances[i]=  bankBalances[i]- bankBalances[i]*0.002
+//      }
+     
+//      else if(bankBalances[i]>100000)
+//      {
 
-    }
+//         bankBalances[i]=  bankBalances[i]+ bankBalances[i]*0.05
+//         bankBalances[i]=  bankBalances[i]- bankBalances[i]*0.0005
+//      }
+
+//      else
+//      {
+//         bankBalances[i]=  bankBalances[i]+ bankBalances[i]*0.03
+//      }
+// }
+
+// //console.log(bankBalances)
+// alert(bankBalance1)
+
+
+// let bankBalance1 = Number(prompt('Enter the first bank balance'))
+// let bankBalance2 = Number(prompt('Enter the first bank balance'))
+// let bankBalance3 = Number(prompt('Enter the first bank balance'))
+// let bankBalance4 = Number(prompt('Enter the first bank balance'))
+// let bankBalance5 = Number(prompt('Enter the first bank balance'))
+
+
+
+
+function calculateInterest()
+{
+   let bankBalance1 = Number(document.getElementById('bank-balance-1').value)
+   let bankBalance2 = Number(document.getElementById('bank-balance-2').value)
+   let bankBalance3 = Number(document.getElementById('bank-balance-3').value)
+   
+  console.log(bankBalance1,bankBalance2,bankBalance3) 
+   
+   
 }
