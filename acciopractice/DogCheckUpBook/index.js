a818c97c6e0b99c238994a2a31790dd0086a9965
Arambh Suyal <arambhsuyal@gmail.com>
2022-05-22 00:12:43 +0530

    new

---
+++ acciopractice/DogCheckUpBook/index.js
@@ -0,0 +1,38 @@
+let show =[]
+
+
+function display()
+
+{
+    if(show.length==0){
+    
+    
+show.push(document.getElementById('i-n').value ) 
+show.push(document.getElementById('i-e').value ) 
+show.push(document.getElementById('i-p').value ) 
+show.push(document.getElementById('i-dn').value)
+show.push(document.getElementById('i-dc').value)
+show.push(document.getElementById('i-da').value)
+// show.push(document.getElementById('i-dbb').value)
+// show.push(document.getElementById('i-dbp').value)
+// show.push(document.getElementById('i-dbm').value)
+
+     console.log(show)
+    }
+
+}
+
+
+// let temp
+
+// let disp = document.getElementsByClassName('input-v')
+
+// // disp = document.getElementsByClassName('input-v').value
+
+// for(let i=0;i<inputBalanceNodes.length;i++)
+//         {
+//        temp   = disp[i].value;
+//         }
+
+// console.log(disp)
+
