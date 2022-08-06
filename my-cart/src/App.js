868bf4679992ca1db70fd9e04fd8f8ba43025164
Arambh Suyal <arambhsuyal@gmail.com>
2022-08-06 14:07:24 +0530

    new

---
+++ my-cart/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+     We are learing React
+    
+    <br/>
+    React is Awesome
+
+    </div>
+
+    </>
+  );
+}
+
+export default App;
