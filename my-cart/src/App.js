3f103e4f2f86f75e19be2d9830165fc37988fe12
Arambh Suyal <arambhsuyal@gmail.com>
2022-11-01 22:28:19 +0530

    new

--- my-cart/src/App.js
+++ my-cart/src/App.js
@@ -1,16 +1,28 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Footer from './components/Footer';
+
 
 function App() {
+  const product =[
+    {
+price:99999,
+name:"Iphone 10s max",
+quantity:0 ,
+    },
+    {
+
+      price:9999,
+      name:"redmi 10s max",
+      quantity:0 ,
+    }
+  ]
   return (
     <>
-    <div className="App">
-     We are learing React
-    
-    <br/>
-    React is Awesome
-
-    </div>
-
+    <Navbar/>
+<ProductList product={product}/>
+{/* <Footer/> */}
     </>
   );
 }
